2e8bea519581978c6d4b4785d4c8d2e8494fb38e
SagorAich <sagoraich1703101@gmail.com>
2022-08-03 22:58:52 +0600

    something added

--- countries.js
+++ countries.js
@@ -12,15 +12,32 @@ const displayCountries=countries=>{
     countries.forEach(country => {
         const div=document.createElement('div');
         div.classList.add('addedAClass')
-        const h3=document.createElement('h3');
+        div.innerHTML=`
+        <h3>${country.name.common}</h3>
+        <p>${country.capital}</p>
+        <button onclick="loadCountryByName('${country.name.common}')">Details</button>
+        `;
+/*         const h3=document.createElement('h3');
         h3.innerText=country.name.common;
         div.appendChild(h3);
-
         const p=document.createElement('p');
         p.innerText=country.capital;
-        div.appendChild(p);
-
-        countriesDiv.appendChild(div);
+        div.appendChild(p); */
+       countriesDiv.appendChild(div);
     });
-
+}
+const loadCountryByName=name=>{
+    const url=`https://restcountries.com/v3.1/name/${name}`;
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>displayCountryDetail(data[0]));
+}
+const displayCountryDetail=country=>{
+    console.log(country);
+    const countryDiv=document.getElementById('country-detail');
+    countryDiv.innerHTML=`
+        <h4>${country.name.common}</h4>
+        <p>population:${country.population}</p>
+        <img width="200px" src="${country.flags.png}">
+    `
 }
